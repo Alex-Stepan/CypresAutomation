69381eacc612bf19d7b5892cf9847debfd63b07b
Alex-Stepan <alexander.stepan@yahoo.com>
2023-05-15 17:39:36 -0400

    First commit

---
+++ cypress/e2e/testStructure.cy.js
@@ -0,0 +1,24 @@
+/// <reference types='cypress' />
+
+describe('Context: Test Structure', () => {
+
+    before(() => {
+
+    });
+    beforeEach(() => {
+        cy.clearCookies();
+    })
+    after(() => {
+
+    })
+    afterEach(() => {
+
+    })
+
+    it('Opening a web application', () => {
+        cy.visit('/registration_form');
+        
+    })
+
+
+})
