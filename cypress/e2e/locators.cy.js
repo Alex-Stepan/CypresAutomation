d4eb6ae9e8da9c27e3024119da04c21c3988a03a
Alex-Stepan <alexander.stepan@yahoo.com>
2023-05-16 16:43:08 -0400

    add: new locators to locators.cy.js

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -19,7 +19,7 @@ describe('Find or Get Elements by using Different Locators', () => {
 
         //for multiple locators:
         //using each loop:
-        
+
         //cy.get("input").each((item, index, list))   //[creates object].[object parameters: item, index, list]
         cy.get("input").each((item, index, list) => {
             //assert the length is 2
@@ -27,6 +27,27 @@ describe('Find or Get Elements by using Different Locators', () => {
             //assert the item has attribute
             expect(item).to.have.attr("type");
         }) 
+
+        //locate by attribute
+        cy.get('[type');
+
+        //locate by class Name
+        cy.get('.btn.btn-primary'); //add a dot in the beginning [btn btn-primary] and for each space char
+
+        //locate by id
+        cy.get('#wooden_spoon');
+
+        //locate by text
+        cy.get('button').should('contain', 'Login').click(); //identify the attribute, then assert if it contain this text
+
+
+
+
+
+
+
+
+
     });
 
 });
