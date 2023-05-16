25575a389eaac915df1e88be1bffeba314d3d8bf
Alex-Stepan <alexander.stepan@yahoo.com>
2023-05-16 16:53:42 -0400

    add: video and retries settings to the cypress.congig.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://practice.cydeo.com/',
+    video: false,
+    retries: 2, //how many times it will retry to reproduce
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
