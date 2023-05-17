41835c76152416194d8c528dcfdb5f0361b588fc
Alex-Stepan <alexander.stepan@yahoo.com>
2023-05-17 12:20:10 -0400

    update: after running eslint & eslint-fix command

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -1,47 +1,43 @@
 /// <reference types="cypress" />
 
 describe('Context: test structure', () => {
-    
-    beforeEach(() => {
-        cy.clearCookies();
-        cy.visit('/multiple_buttons');
-        //runs before each test
-    });
-    
-    it('Check Different Button Actions', () => {
-        //1. select a button with text
-        cy.contains('Button 2').should('be.visible').click();
-        cy.contains('Clicked on button two!').should('be.visible');
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.visit('/multiple_buttons');
+    // runs before each test
+  });
 
-        //2. find element by class attribute, create a list, select 3rd element
-        //by class attribute: need a dot in front[btn btn-primary]-->[.btn.btn-primary]
-        cy.get('.btn.btn-primary').then(($buttons) => {
-            cy.wrap($buttons).eq(2).click();    //eq(2) - means 0,1,2...element in the list
-            //  List in JS    get() 
-            //assert the text
-            cy.contains('Clicked on button three!').should('be.visible');
-        })
+  it('Check Different Button Actions', () => {
+    // 1. select a button with text
+    cy.contains('Button 2').should('be.visible').click();
+    cy.contains('Clicked on button two!').should('be.visible');
 
-        // 3. get all buttons with tagname[button], each method is creating a loop,
-        cy.get('button').each((item, index, list) => {
-            //assert length of the list, verify number of buttons
-            expect(list).to.have.length(6);         //to have length 6
-            expect(item).to.have.attr("onclick");   //to have each item attribute [onclick]
-        })
-
-        //4. get all buttons, get only the item and check for text of each item, if it will equal 4, then click on it
-        cy.get('button').each((item) => {
-            if(item.text() == "Button 4"){
-                cy.log(item.text());    //this command write the text at the test console
-                //item.click(); - this click() would not work on jQuery element
-                cy.wrap(item).click();
-                cy.contains('Clicked on button four!').should('be.visible');
-
-            }
-        })
-            // npx sypress run --headless -b chrome
-            // this is the command to run all test headlees in chrome
-
+    // 2. find element by class attribute, create a list, select 3rd element
+    // by class attribute: need a dot in front[btn btn-primary]-->[.btn.btn-primary]
+    cy.get('.btn.btn-primary').then(($buttons) => {
+      cy.wrap($buttons).eq(2).click(); // eq(2) - means 0,1,2...element in the list
+      //  List in JS    get()
+      // assert the text
+      cy.contains('Clicked on button three!').should('be.visible');
     });
 
+    // 3. get all buttons with tagname[button], each method is creating a loop,
+    cy.get('button').each((item, index, list) => {
+      // assert length of the list, verify number of buttons
+      expect(list).to.have.length(6); // to have length 6
+      expect(item).to.have.attr('onclick'); // to have each item attribute [onclick]
+    });
+
+    // 4. get all buttons, get only the item and check for text of each item, if it will equal 4, then click on it
+    cy.get('button').each((item) => {
+      if (item.text() == 'Button 4') {
+        cy.log(item.text()); // this command write the text at the test console
+        // item.click(); - this click() would not work on jQuery element
+        cy.wrap(item).click();
+        cy.contains('Clicked on button four!').should('be.visible');
+      }
+    });
+    // npx sypress run --headless -b chrome
+    // this is the command to run all test headlees in chrome
+  });
 });
--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -1,63 +1,64 @@
 /// <reference types="cypress" />
 
 describe('Find or Get Elements by using Different Locators', () => {
-    beforeEach(() => {
-        //runs before each test
-        cy.clearCookies();
-        cy.visit('/login');
+  beforeEach(() => {
+    // runs before each test
+    cy.clearCookies();
+    cy.visit('/login');
+  });
+
+  it('Check different locators strategy', () => {
+    // By CSS locators
+    cy.get("input[name='username']").clear(); // [creates an object].[operation to the object]
+    cy.get("input[name='username']").type('CydeoStudent');
+    cy.get("input[name='username']").clear();
+
+    cy.get("input[name='password']").clear();
+    cy.get("input[name='password']").type('CydeoPassword');
+    cy.get("input[name='password']").clear();
+
+    // for multiple locators:
+    // using each loop:
+
+    // locate by tag name
+    // cy.get("input").each((item, index, list))   //[creates object].[object parameters: item, index, list]
+    cy.get('input').each((item, index, list) => {
+      // assert the length is 2
+      expect(list).to.have.length(2);
+      // assert the item has attribute
+      expect(item).to.have.attr('type');
     });
 
-    it('Check different locators strategy', () => {
-        //By CSS locators
-        cy.get("input[name='username']").clear(); //[creates an object].[operation to the object]
-        cy.get("input[name='username']").type("CydeoStudent");
-        cy.get("input[name='username']").clear();
+    // locate by attribute
+    cy.get('[type');
 
-        cy.get("input[name='password']").clear();
-        cy.get("input[name='password']").type("CydeoPassword");
-        cy.get("input[name='password']").clear();
+    // locate by class Name
+    cy.get('.btn.btn-primary'); // add a dot in the beginning [btn btn-primary] and for each space char
 
-        //for multiple locators:
-        //using each loop:
+    // locate by id
+    cy.get('#wooden_spoon');
 
-        //locate by tag name
-        //cy.get("input").each((item, index, list))   //[creates object].[object parameters: item, index, list]
-        cy.get("input").each((item, index, list) => {
-            //assert the length is 2
-            expect(list).to.have.length(2);
-            //assert the item has attribute
-            expect(item).to.have.attr("type");
-        }) 
+    // locate by text
+    cy.get('button').should('contain', 'Login').click(); // identify the attribute, then assert if it contain this text
+  });
 
-        //locate by attribute
-        cy.get('[type');
+  it('check Finding Elemnts by traveling through DOM', () => {
+    // travel to find the login button: locate username box > go to parent form > find button
+    cy.get("input[name='username']")
+      .parents('form')
+      .find('button')
+      .should('contain', 'Login')
+      .click();
+  });
 
-        //locate by class Name
-        cy.get('.btn.btn-primary'); //add a dot in the beginning [btn btn-primary] and for each space char
+  it.only('Check Different Type of Assertions', () => {
+    // Should Assertion
+    cy.get('#wooden_spoon').should('contain', 'Login').and('have.class', 'btn btn-primary');
 
-        //locate by id
-        cy.get('#wooden_spoon');
-
-        //locate by text
-        cy.get('button').should('contain', 'Login').click(); //identify the attribute, then assert if it contain this text
+    // expect Assertion (creates object, then is doing actions to the object)
+    cy.get('#wooden_spoon').then((buttonElement) => {
+      expect(buttonElement).to.have.text('Login');
+      expect(buttonElement).to.have.class('btn btn-primary');
     });
-
-    it('check Finding Elemnts by traveling through DOM', () => {
-        //travel to find the login button: locate username box > go to parent form > find button
-        cy.get("input[name='username']").parents('form').find('button').should('contain', 'Login').click();
-    });
-
-    it.only('Check Different Type of Assertions', () => {
-        //Should Assertion
-        cy.get('#wooden_spoon')
-        .should('contain', 'Login')
-        .and('have.class', 'btn btn-primary');
-        
-        //expect Assertion (creates object, then is doing actions to the object)
-        cy.get('#wooden_spoon').then((buttonElement) =>{
-            expect(buttonElement).to.have.text('Login');
-            expect(buttonElement).to.have.class('btn btn-primary');
-        })
-    });
-
+  });
 });
