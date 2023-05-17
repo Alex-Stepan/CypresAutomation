b77e46f3a7545e52feace59d25f70dc8a6ffdbbe
Alex-Stepan <alexander.stepan@yahoo.com>
2023-05-17 11:09:39 -0400

    add: wrap List in each loop inside buttons.cy.js

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -9,11 +9,11 @@ describe('Context: test structure', () => {
     });
     
     it('Check Different Button Actions', () => {
-        //select a button with text
+        //1. select a button with text
         cy.contains('Button 2').should('be.visible').click();
         cy.contains('Clicked on button two!').should('be.visible');
 
-        //find element by class attribute, create a list, select 3rd element
+        //2. find element by class attribute, create a list, select 3rd element
         //by class attribute: need a dot in front[btn btn-primary]-->[.btn.btn-primary]
         cy.get('.btn.btn-primary').then(($buttons) => {
             cy.wrap($buttons).eq(2).click();    //eq(2) - means 0,1,2...element in the list
@@ -21,6 +21,25 @@ describe('Context: test structure', () => {
             //assert the text
             cy.contains('Clicked on button three!').should('be.visible');
         })
+
+        // 3. get all buttons with tagname[button], each method is creating a loop,
+        cy.get('button').each((item, index, list) => {
+            //assert length of the list, verify number of buttons
+            expect(list).to.have.length(6);         //to have length 6
+            expect(item).to.have.attr("onclick");   //to have each item attribute [onclick]
+        })
+
+        //4. get all buttons, get only the item and check for text of each item, if it will equal 4, then click on it
+        cy.get('button').each((item) => {
+            if(item.text() == "Button 4"){
+                cy.log(item.text());    //this command write the text at the test console
+                //item.click(); - this click() would not work on jQuery element
+                cy.wrap(item).click();
+                cy.contains('Clicked on button four!').should('be.visible');
+
+            }
+        })
+
 
     });
 
