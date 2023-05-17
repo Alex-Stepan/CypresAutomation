af74e29c3cfaddbd0926f4af5a23c573f2fa9185
Alex-Stepan <alexander.stepan@yahoo.com>
2023-05-17 10:55:26 -0400

    add: wrap List and get method inside  buttons.cy.js

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -12,6 +12,16 @@ describe('Context: test structure', () => {
         //select a button with text
         cy.contains('Button 2').should('be.visible').click();
         cy.contains('Clicked on button two!').should('be.visible');
+
+        //find element by class attribute, create a list, select 3rd element
+        //by class attribute: need a dot in front[btn btn-primary]-->[.btn.btn-primary]
+        cy.get('.btn.btn-primary').then(($buttons) => {
+            cy.wrap($buttons).eq(2).click();    //eq(2) - means 0,1,2...element in the list
+            //  List in JS    get() 
+            //assert the text
+            cy.contains('Clicked on button three!').should('be.visible');
+        })
+
     });
 
 });
