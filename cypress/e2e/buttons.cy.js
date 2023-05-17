a44f6f2db73af6867ea76eacc80d101e22dd0e73
Alex-Stepan <alexander.stepan@yahoo.com>
2023-05-17 08:58:10 -0400

    add: default Command Timeout to cypress.config.js

---
+++ cypress/e2e/buttons.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('Context: test structure', () => {
+    
+    beforeEach(() => {
+        cy.clearCookies();
+        cy.visit('/multiple_buttons/');
+        //runs before each test
+    });
+    
+    it('Check Different Button Actions', () => {
+        //select a button with text
+        cy.contains('Button 2').should('be.visible').click();
+        cy.contains('Clicked on button two!').should('be.visible');
+    });
+
+});
