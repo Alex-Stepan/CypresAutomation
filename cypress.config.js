e0f44423e0086ff08be9416d9ba3379c39198b8c
Alex-Stepan <alexander.stepan@yahoo.com>
2023-05-17 09:00:46 -0400

    add: Timeout change to 5 sec

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://practice.cydeo.com/',
-    video: false,
+    video: false, //do not recored video
     retries: 2, //how many times it will retry to reproduce
+    defaultCommandTimeout: 5000,  //waiting timeout (by default is 4sec=4000msec)
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
